42619a26050357dac65b08725277cafb82e5853b
voronKutha <foxthread@outlook.com>
2024-08-07 10:55:08 +1000

    adding linksGetter init in run method

--- src/main/java/parser/AppLauncher.java
+++ src/main/java/parser/AppLauncher.java
@@ -1,22 +1,33 @@
 package parser;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import parser.engine.PagesLinksGetter;
 
 @SpringBootApplication
-public class AppLauncher  implements CommandLineRunner{
+public class AppLauncher implements CommandLineRunner {
 
-    public static void main(String args[])  {
-          SpringApplication.run(AppLauncher.class, args);
-      
+    @Autowired
+    private PagesLinksGetter linksGetter;
+
+    public static void main(String args[]) {
+        SpringApplication.run(AppLauncher.class, args);
+
     }
 
     @Override
     public void run(String... args) throws Exception {
-       
+        
+        linksGetter
+                .setBaseUrl("https://www.farpost.ru/khabarovsk/realty/sell_houses")
+                .setCssExpression("a[data-role*=bulletin-link]")
+                .setPagePrefix("?page=")
+                .setFirstPage(1)
+                .setLastPage(1);
+
     }
 
 }
